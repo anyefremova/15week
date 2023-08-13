1357d5b6294ec24c35961ee57d9f712f590336e1
anyefremova <anna.efremova.02@yandex.ru>
2023-08-14 03:42:18 +0500

    сделала пункты 1-5

--- task 2/index.js
+++ task 2/index.js
@@ -1,6 +1,21 @@
 const cities = ['Пермь', 'Москва', 'Екатеринбург', 'Тюмень'];
 let temperatures = [];
 
-for (let i = 0; i < cities.length; i++) {
+const mainBlock = document.querySelector('.main__block');
+const temperaturesInCity = document.createElement('ul');
+mainBlock.appendChild(temperaturesInCity);
+const max = document.getElementsByClassName('max__temp');
+const min = document.getElementsByClassName('min__temp');
 
+for (let i = 0; i < cities.length; i++) {
+    let temperature = prompt(`Введите температуру в городе ${cities[i]}`);
+    temperatures.push(Number(temperature));
 }
+console.log(temperatures);
+
+for (let i = 0; i < cities.length; i++) {
+    const item = document.createElement('li');
+    item.textContent = `Температура в ${cities[i]}: ${temperatures[i]} °С`;
+    temperaturesInCity.appendChild(item);
+}
+
